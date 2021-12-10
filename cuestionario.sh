29691f99c8546ef8f6cc599980bae86c53bd2145
root <root@debian11.lan>
2021-12-10 13:48:23 -0600

    many changes

--- cuestionario.sh
+++ cuestionario.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
+clear
+############################# Funcion de mayusculas
+function mayus() {
+	DATO=$(echo $* | awk '{print toupper($0)}')
+	echo $DATO
+}
+
+############################# Funcion de minusculas
+function minus() {
+        DATO=$(echo $* | awk '{print tolower($0)}')
+        echo $DATO
+}
 
 echo "########################### Formulario de cliente ##################"
 echo
 #************************NOMBRE
 echo -n "Ingrese su nombre: "
 read nombre
-until [[ $nombre =~ ^[a-zA-Zá-úÁ-ÚÑñ]{2,10}( {1}[a-zA-Zá-úÁ-ÚÑñ]{4,10})+$ ]]
+until [[ $nombre =~ ^[a-zA-Zá-úÁ-ÚÑñ]{2,10}( {1}[a-zA-Zá-úÁ-ÚÑñ]{2,10})+$ ]]
 do
 	echo "!! Recuerda no ingresar mas de un espacio entre nombres y apellidos, no ingresar números.";echo "Al menos debe de tener 6 caracteres tu nombre !!";sleep 5
 	echo -n "Vuelva a ingresar su nombre completo: "
-	read nombre 
+	read nombre
 done
+sleep 3
+echo
 
-NOMBREC=$(echo $nombre | awk '{print toupper($0)}')
-echo $NOMBREC;sleep 3
-echo 
 #*************************DOMICILIO
 echo "Ingrese su Domicilio"
 echo
@@ -23,36 +34,45 @@ echo -n "Calle: "
 read calle
 until [[ $calle =~ ^[a-zA-Zá-úÁ-ÚÑñ]( ?[a-zA-Zá-úÁ-ÚÑñ0-9,\-\.]){2,149}$ ]]
 do
-        echo "!! Recuerda ingresar una calle minima de 10 caracteres de longitud.";echo "Solo se acepta espacios, comas, puntos y guiones medios !!";sleep 3
+        echo "!! Recuerda ingresar una calle minima de 3 caracteres de longitud.";echo "Solo se acepta espacios, comas, puntos y guiones medios !!";sleep 3
         echo -n "Vuelva a ingresar la calle: "
         read calle
 done
-
-CALLE2=$(echo $calle | awk '{print toupper($0)}')
-echo $CALLE2; sleep 2
+sleep 2
 echo
+
 ###############################Numero exterior
 echo -n "Numero exterior/Manzana (en caso de no tener ingrese SN): "
 read numext
-until [[ $numext =~ ^[0-9]{1,5}$ ]] || [[ $numext = "SN" ]]
+until [[ $numext =~ ^[0-9]{1,5}(\-[A-Za-zÑñ])?$ ]] || [[ $numext = "SN" ]]
 do
         echo -n "Vuelva a ingresar el numero exterior/manzana: "
         read numext
 done
-
-echo $numext;sleep 2
+if [[ $numext = "SN" ]];then
+	exterior=""
+else 
+	exterior="$numext"
+fi
+sleep 2
 echo
+
 ##############################Numero interior
 echo -n "Numero interior/Lote (en caso de no tener ingrese SN): "
 read numint
-until [[ $numint =~ ^[0-9]{1,3}$ ]] || [[ $numint = "SN" ]]
+until [[ $numint =~ ^[0-9]{1,3}(\-[A-Za-zÑñ])?$ ]] || [[ $numint = "SN" ]]
 do
         echo -n "Vuelva a ingresar el numero interior/lote: "
         read numint
 done
-
-echo $numint;sleep 2
+if [[ $numint = "SN" ]];then
+        interior=""
+else
+        interior="$numext"
+fi
+sleep 2
 echo
+
 ############################Colonia
 echo -n "Colonia/Asentamiento/Región: "
 read colonia
@@ -62,9 +82,9 @@ do
 	echo -n "Vuelva a ingresar la colonia/asentamiento/región: "
         read colonia
 done
-COLONIA2=$(echo $colonia | awk '{print toupper($0)}')
-echo $COLONIA2; sleep 2
+sleep 2
 echo
+
 ###########################Alcaldia
 echo -n "Alcaldía/Municipio: "
 read alcaldia
@@ -74,9 +94,9 @@ do
         echo -n "Vuelva a ingresar alcaldía/municipio: "
         read alcaldia
 done
-ALCAL=$(echo $alcaldia | awk '{print toupper($0)}')
-echo $ALCAL; sleep 2
+sleep 2
 echo
+
 ##########################Ciudad
 echo -n "Ciudad/Estado: "
 read estado
@@ -86,9 +106,9 @@ do
         echo -n "Vuelva a ingresar estado/ciudad: "
         read estado
 done
-ESTADO2=$(echo $estado | awk '{print toupper($0)}')
-echo $ESTADO2; sleep 2
+sleep 2
 echo
+
 ##########################Pais
 echo -n "País: "
 read pais
@@ -98,9 +118,9 @@ do
         echo -n "Vuelva a ingresar pais: "
         read pais
 done
-PAIS2=$(echo $pais | awk '{print toupper($0)}')
-echo $PAIS2; sleep 2
+sleep 2
 echo
+
 ########################CP
 echo -n "Código Postal: "
 read cp
@@ -115,25 +135,29 @@ if [[ $cp =~ ^[0-9]{4}$ ]];then
 else
 	codigo="$cp"
 fi
-
-echo $codigo; sleep 2
+sleep 2
 echo
+
 #**************************Correo y Verificacion
 echo -n "Ingrese su correo electrónico: "
 read correo
+until [[ $correo =~ ^[a-zA-Zá-úÁ-ÚÑñ0-9][a-zA-Zá-úÁ-ÚÑñ0-9,\_\.\-]{0,18}[a-zA-Zá-úÁ-ÚÑñ0-9]@[a-zA-Zá-úÁ-ÚÑñ0-9]{1,20}(\.[a-zA-Zá-úÁ-ÚÑñ0-9]{1,10})?(\.[a-zA-Zá-úÁ-ÚÑñ]{2,3})$ ]]
+do
+       	echo "!! Recuerda no ingresar espacios, ni tabulaciones, se permiten -, ., _ y ,"
+       	echo -n "Ingresar el correo: "
+       	read correo
+done
 echo -n "Confirmación de correo, ingrese su correo: "
 read vcorreo
-until [[ $correo =~ ^[a-zA-Zá-úÁ-ÚÑñ0-9][a-zA-Zá-úÁ-ÚÑñ0-9,\_\.\-]{0,18}[a-zA-Zá-úÁ-ÚÑñ0-9]@[a-zA-Zá-úÁ-ÚÑñ0-9]{1,20}(\.[a-zA-Zá-úÁ-ÚÑñ0-9]{1,10})?(\.[a-zA-Zá-úÁ-ÚÑñ]{2,3})+$ ]] && [[ $correo = $vcorreo ]]
+until [[ $correo = $vcorreo ]]
 do
-        echo "!! Recuerda no ingresar espacios, ni tabulaciones, se permiten -, ., _ y ,"
-	echo "Ambos correos deben ser iguales"
-        echo -n "Ingresar el correo: "
-        read correo
-	echo -n "Ingrese su correo para confirmar: "
+	echo "!!Recuerda que el correo y la confirmación de correo deben ser iguales"
+	echo -n "Ingrese la confirmación de nuevo: "
 	read vcorreo
 done
-echo $correo;sleep 2
+sleep 2
 echo
+
 #**************************Telefono
 echo -n "Ingrese su número de teléfono: "
 read  telefono
@@ -143,9 +167,9 @@ do
 	echo -n "Vuelva a ingresar el número de teléfono: "
         read telefono
 done
-
-echo $telefono;sleep 2
+sleep 2
 echo
+
 #************************Usuario
 echo -n "Ingrese el nombre de usuario(se permiten letras, numeros, puntos y guion): "
 read usuario
@@ -156,23 +180,42 @@ do
 	echo -n "Vuelva a ingresar el usuario: "
         read usuario
 done
-
-USUARIO2=$(echo $usuario | awk '{print tolower($0)}')
-echo $USUARIO2; sleep 2
+sleep 2
 echo
+
 #*********************Contraseña
 echo -n "Ingrese su contraseña(se permiten letras, numeros,guiones,puntos,#,$,&,%,*,@): "
 read pass
-echo -n "Confirmación de contraseña, ingrese su contraseña: "
-read vpass
-until [[ $pass =~ ^[a-zA-Zá-úÁ-ÚÑñ0-9@\#\$\&\*\%\_\.\-]{8,40}$ ]] && [[ $pass = $vpass ]]
+until [[ $pass =~ ^[a-zA-Zá-úÁ-ÚÑñ0-9@\#\$\&\*\%\_\.\-]{8,40}$ ]]
 do
         echo "!! Recuerda la contraseña minima es 8 y maxima de 40 caracteres"
-        echo "Ambos contraseñas deben ser igual"
         echo -n "Ingresar la contraseña: "
         read pass
-        echo -n "Ingrese su contraseña para confirmar: "
+done
+echo -n "Confirmación de contraseña, ingrese su contraseña: "
+read vpass
+until [[ $pass = $vpass ]]
+do
+        echo "!! Ambas contraseñas deben ser igual"
+        echo -n "Ingrese la confirmación de contraseña: "
         read vpass
 done
-echo $pass;sleep 2
+sleep 2
 echo
+echo "Cargando los datos ...."
+sleep 4
+clear
+###############Impresion de datos
+echo -n "Nombre: ";mayus $nombre
+echo -n "Calle: ";mayus $calle
+echo -n "Número exterior: ";mayus $exterior
+echo -n "Número interior: ";mayus $interior
+echo -n "Colonia: ";mayus $colonia
+echo -n "Alcadía/Municipio: ";mayus $alcaldia
+echo -n "Estado: ";mayus $estado
+echo -n "País: ";mayus $pais
+echo -n "Código: ";echo $codigo
+echo -n "Correo: ";mayus $correo
+echo -n "Teléfono: ";echo $telefono
+echo -n "Usuario: ";minus $usuario
+echo -n "Contraseña: ";echo $pass
